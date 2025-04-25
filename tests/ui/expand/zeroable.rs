278070d12b41f0fca1b0d6dff6c5f38f3cf46044
Benno Lossin <benno.lossin@proton.me>
2025-04-25 11:47:53 +0200

    tests: add expand and compile-fail tests for `[Maybe]Zeroable`
    Signed-off-by: Benno Lossin <benno.lossin@proton.me>

---
+++ tests/ui/expand/zeroable.rs
@@ -0,0 +1,21 @@
+use pin_init::*;
+
+#[derive(Zeroable)]
+struct Foo {
+    a: usize,
+    pub(crate) b: usize,
+}
+
+#[derive(MaybeZeroable)]
+struct Bar {
+    a: usize,
+    b: &'static usize,
+}
+
+trait Trait {}
+
+#[derive(Zeroable)]
+struct WithGenerics<'a, T, U: Trait> {
+    a: T,
+    u: &'a U,
+}
